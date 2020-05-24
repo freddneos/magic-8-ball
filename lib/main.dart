018af73652101d4560221d03042c5609990eedc9
Frederico Bezerra <frederico.bezerra@definedcrowd.com>
2020-05-24 16:11:39 +0100

    finishing project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -17,7 +18,33 @@ class BallPage extends StatelessWidget {
         title: Text("Ask me Anything"),
       ),
       backgroundColor: Colors.blue[300],
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  void ballRandom() {
+    setState(() {
+      ballNumber = Random().nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {
+          ballRandom();
+        },
+      ),
     );
   }
 }
